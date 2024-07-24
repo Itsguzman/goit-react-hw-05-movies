4f7dcc7a93d22aecaae46f8d94e761b2b136b63c
Itsguzman <147312123+Itsguzman@users.noreply.github.com>
2024-07-24 13:15:25 +0000

    Deploying to gh-pages from @ Itsguzman/goit-react-hw-05-movies@dde8391bbd3bbdb690551942b89b2b9044c7054b 🚀

---
+++ static/js/692.4bd29509.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[692],{692:function(e,t,r){r.r(t);r(791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"No review at this moment."})}}}]);
+//# sourceMappingURL=692.4bd29509.chunk.js.map
